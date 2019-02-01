1171d4e56d7c559ffe611f906f87144b401683c7
puth1998 <teenaputh12@hotmail.com>
2019-02-01 17:51:32 +0700

    xx

--- src/calgrade/GradeDemo.java
+++ src/calgrade/GradeDemo.java
@@ -3,8 +3,13 @@ package calgrade;
 public class GradeDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Input input = new Input();
+		Process process;
+		Output output = new Output();
+		int score = input.inInt();
+		process = new Process(score);
+		output.print(process.calculate());
+		
+		}
 
 }
