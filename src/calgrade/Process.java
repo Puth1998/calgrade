8fae43dfb3b9769a64ffacacb029575d34dafa23
Fahg2541 <Fahg2541@gmail.com>
2019-02-01 17:52:57 +0700

    11

--- src/calgrade/Process.java
+++ src/calgrade/Process.java
@@ -8,7 +8,18 @@ public class Process {
 	}
 	
 	public char calculate() {
-		return (Character) null;
+		char grade;
+		if( score >= 80 ) {
+			grade = 'A';
+		}else if ( score >= 70 ) {
+			grade = 'B';
+		}else if ( score >= 60 ) {
+			grade = 'C';
+		}else if ( score >= 50 ) {
+			grade = 'D';
+		}else
+			grade = 'F';
+		return grade;
 	}
 
 }
