9fb656104ebf93876c4c62f44fa9521ccf8c56c3
puth1998 <teenaputh12@hotmail.com>
2019-02-01 16:49:57 +0700

    src

---
+++ src/calgrade/GradeDemo.java
@@ -0,0 +1,10 @@
+package calgrade;
+
+public class GradeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/calgrade/Output.java
@@ -0,0 +1,5 @@
+package calgrade;
+
+public class Output {
+
+}
